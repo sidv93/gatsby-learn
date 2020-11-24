00c8c0ac5158a50ad10efedc8c724b3242ee513b
Siddharth Venkatesh <32375112+sidv93@users.noreply.github.com>
2020-11-24 23:52:49 +0530

    global styles

--- ui/src/components/Layout.js
+++ ui/src/components/Layout.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
-const Layout = ({children}) => (
+const Layout = ({ children }) => (
   <div>
+    <GlobalStyles />
+    <Typography />
     <Nav />
     {children}
     <Footer />
