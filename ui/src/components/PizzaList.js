5ac58747f8fb7367e981ddc2f53cdf59a9e39e7c
Siddharth Venkatesh <32375112+sidv93@users.noreply.github.com>
2020-12-02 00:04:04 +0530

    gatsby images and pizza grid

--- ui/src/components/PizzaList.js
+++ ui/src/components/PizzaList.js
@@ -1,23 +1,47 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const PizzaGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
+
+const PizzaContainer = styled.div`
+  display: grid;
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 1fr;
+  }
+  grid-template-rows: var(--rows, subgrid);
+  grid-row: span 3;
+  grid-gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 
 const Pizza = ({ pizza }) => (
-  <div>
+  <PizzaContainer>
     <Link to={`/pizza/${pizza.slug.current}`}>
       <h2>
         <span className="mark">{pizza.name}</span>
       </h2>
-      <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
     </Link>
-  </div>
+    <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+    <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
+  </PizzaContainer>
 );
 
 const PizzaList = ({ pizzas }) => (
-  <div>
+  <PizzaGrid>
     {pizzas.map((pizza) => (
       <Pizza pizza={pizza} key={pizza.id} />
     ))}
-  </div>
+  </PizzaGrid>
 );
 
 export default PizzaList;
